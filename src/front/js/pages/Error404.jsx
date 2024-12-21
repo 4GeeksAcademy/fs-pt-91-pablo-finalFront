8f1373ca86ba027e520b97b5a3e4e6a854a0d372
Pablo Enguix Llopis <56578000+PabloEnguix09@users.noreply.github.com>
2024-12-21 08:58:29 +0000

    hecho en clase

---
+++ src/front/js/pages/Error404.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Error404 = () => {
+    return (
+        <div className="container mt-5">
+            <h1 className="display-1 text-danger text-center">Page Not found</h1>
+        </div>
+    )
+}
